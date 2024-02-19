8e626917ab7762218566444f02514ab093daebc1
Max Gusenbauer <83061202+max-gusenb-it@users.noreply.github.com>
2024-02-19 09:54:41 +0100

    small changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ALaCard',
-  webDir: 'www',
+  webDir: 'public',
   server: {
     androidScheme: 'https'
   }
