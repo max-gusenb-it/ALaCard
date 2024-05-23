237cd2b5973f51b8e8eb49d48d23157c2bca6efd
Max Gusenbauer <83061202+max-gusenb-it@users.noreply.github.com>
2024-05-23 14:47:20 +0200

    fix(navigation): router navigation padding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
+import { ItNavigationComponent } from './shared/components/buttons/it-navigation/it-navigation.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements AfterViewInit {
+  @ViewChild(ItNavigationComponent) navigation?: ItNavigationComponent;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    // after view init the scroll height of the navigation element changes and thus the padding of the router changes,
+    // so change detection has to be triggered
+    this.changeDetectorRef.detectChanges();
+  }
+
+  getNavigationHeight() {
+    const navContainerElement = this.navigation?.containerRef?.nativeElement;
+    if (!!navContainerElement) {
+      let margin = window.getComputedStyle(navContainerElement).marginBottom;
+      return navContainerElement.scrollHeight + (!!margin ? parseInt(margin.replace('px', '')) : 0);
+    } else {
+      return 0;
+    }
+  }
 }
--- src/app/shared/components/Buttons/it-navigation/it-navigation.component.ts
+++ src/app/shared/components/Buttons/it-navigation/it-navigation.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'it-navigation',
   templateUrl: './it-navigation.component.html',
 })
 export class ItNavigationComponent {
+  @ViewChild("navigation") containerRef?: ElementRef<HTMLDivElement>;
 
   activeTab: string = "home";
 
