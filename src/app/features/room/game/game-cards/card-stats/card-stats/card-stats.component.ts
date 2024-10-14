d20c2d3b7353cae885db7d248126bbcbbadff019
Max Gusenbauer <83061202+max-gusenb-it@users.noreply.github.com>
2024-10-14 11:30:44 +0200

    feat(game): component generation

---
+++ src/app/features/room/game/game-cards/card-stats/card-stats/card-stats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-stats',
+  templateUrl: './card-stats.component.html',
+})
+export class CardStatsComponent {
+
+  constructor() { }
+
+}
---
+++ src/app/features/room/game/game-cards/card-stats/player-voting-stats/player-voting-stats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-voting-stats',
+  templateUrl: './player-voting-stats.component.html'
+})
+export class PlayerVotingStatsComponent {
+
+  constructor() { }
+
+}
