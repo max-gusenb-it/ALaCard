24c9335a7501e08055de0c75841a19233efc944b
Max Gusenbauer <83061202+max-gusenb-it@users.noreply.github.com>
2024-11-29 14:48:10 +0100

    feat(game-history-entry): added navigation

--- src/app/features/home/game-history-entry/game-history-entry.component.ts
+++ src/app/features/home/game-history-entry/game-history-entry.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input } from "@angular/core";
+import { Component, HostListener, Input } from "@angular/core";
+import { NavController } from "@ionic/angular";
+import { TranslateService } from "@ngx-translate/core";
+import { firstValueFrom } from "rxjs";
 import { GameHistoryEntry } from "src/app/core/models/interfaces";
+import { PopupService } from "src/app/core/services/service/popup.service";
 
 @Component({
     selector: 'game-history-entry',
@@ -7,4 +11,23 @@ import { GameHistoryEntry } from "src/app/core/models/interfaces";
 })
 export class GameHistoryEntryComponent {
     @Input() gameHistoryEntry: GameHistoryEntry;
+
+    constructor(
+        private navController: NavController,
+        private popupService: PopupService,
+        private translateService: TranslateService,
+
+    ) {}
+
+    @HostListener("click") onClick(){
+        firstValueFrom(this.popupService.openOptionBottomSheet(
+            `${this.translateService.instant("features.home.game-history-entry.join-dialog.title-1")} ${this.gameHistoryEntry.roomCreatorUsername}${this.translateService.instant("features.home.game-history-entry.join-dialog.title-2")}`,
+            this.translateService.instant("actions.cancel"),
+            this.translateService.instant("actions.join"),
+        ).closed).then(joinRoom => {
+            if (joinRoom) {
+                this.navController.navigateForward(`/room/${this.gameHistoryEntry.roomCreatorId}-${this.gameHistoryEntry.roomId}`);
+            }
+        })
+    }
 }
