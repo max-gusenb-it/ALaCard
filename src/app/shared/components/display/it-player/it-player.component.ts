b2de3c5eddaaea5016fc163f11e635b8ce066281
Max Gusenbauer <83061202+max-gusenb-it@users.noreply.github.com>
2025-01-13 11:36:55 +0100

    feat(it-player): design for kick button

--- src/app/shared/components/display/it-player/it-player.component.ts
+++ src/app/shared/components/display/it-player/it-player.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { PlayerState } from 'src/app/core/models/enums';
+import { RoomService } from 'src/app/core/services/service/room.service';
 
 @Component({
   selector: 'it-player',
@@ -9,12 +10,18 @@ import { PlayerState } from 'src/app/core/models/enums';
 })
 export class ItPlayerComponent {
 
+  @ViewChild("player") playerContainerRef?: ElementRef<HTMLDivElement>;
+  @ViewChild("controls") controlsContainerRef?: ElementRef<HTMLDivElement>;
+
   @Input() profilePicture: string = "";
   @Input() username: string = "";
   @Input() state: PlayerState = null as any;
 
+  displayControls: boolean = false;
+
   constructor(
-    private translateService: TranslateService 
+    private translateService: TranslateService,
+    private roomService: RoomService
   ) { }
 
   getStateTranslation(state: PlayerState) {
@@ -26,4 +33,19 @@ export class ItPlayerComponent {
     }
   }
 
+  toggleControls() {
+    if (!!!this.controlsContainerRef || !this.roomService.isUserAdmin()) return;
+
+    this.displayControls = !this.displayControls;
+    
+    if (this.displayControls) {
+      this.controlsContainerRef!.nativeElement.style.maxWidth = this.controlsContainerRef!.nativeElement.scrollWidth + "px";
+    } else {
+      this.controlsContainerRef!.nativeElement.style.maxWidth = "0px";
+    }
+  }
+
+  onMouseLeave() {
+    if (this.displayControls) this.toggleControls();
+  }
 }
