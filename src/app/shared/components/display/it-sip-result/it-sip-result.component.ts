84d0e402c2069d44430fb37ea8d1b5dbe9b00912
Max Gusenbauer <83061202+max-gusenb-it@users.noreply.github.com>
2025-01-27 14:39:11 +0100

    feat(it-sip-result): added message on click

--- src/app/shared/components/display/it-sip-result/it-sip-result.component.ts
+++ src/app/shared/components/display/it-sip-result/it-sip-result.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from "@angular/core";
-import { SipResult } from "src/app/core/models/interfaces";
+import { TranslateService } from "@ngx-translate/core";
+import { Player, SipResult } from "src/app/core/models/interfaces";
+import { PopupService } from "src/app/core/services/service/popup.service";
 
 @Component({
   selector: 'it-sip-result',
@@ -7,6 +9,32 @@ import { SipResult } from "src/app/core/models/interfaces";
   styleUrl: './it-sip-result.component.scss'
 })
 export class ItSipResultComponent {
-    @Input() profilePicture: string;
-    @Input() sipResult: SipResult;
+  @Input() sipResult: SipResult;
+  @Input() player: Player;
+
+  constructor(
+    private translateService: TranslateService,
+    private popUpService: PopupService
+  ) { }
+
+  openSnackbar() {
+    let action = "";
+    if (this.sipResult.distribute) {
+      action = this.translateService.instant("shared.components.display.it-sip-result.distribution");
+    } else {
+      action = this.translateService.instant("shared.components.display.it-sip-result.drink");
+    }
+
+    const sip = this.translateService.instant("shared.components.display.it-result.sip");
+    const sips = this.translateService.instant("shared.components.display.it-result.sips");
+    
+
+    this.popUpService.openSnackbar(
+      `${this.player.username} ${action} ${this.sipResult.sips} ${this.sipResult.sips > 1 ? sips : sip}`,
+      undefined,
+      true,
+      true,
+      2000
+    );
+  }
 }
